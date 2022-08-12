5ac985583af4aebdb484779b5988eee5149bf237
Lee <leedaero777@gmail.com>
2022-08-12 22:15:50 +0900

    2022-08-12 게시판

---
+++ src/main/java/com/godcoder/myhome/controller/UserApiController.java
@@ -0,0 +1,73 @@
+package com.godcoder.myhome.controller;
+
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.model.User;
+import com.godcoder.myhome.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+class UserApiController {
+    @Autowired
+    private final UserRepository repository;
+
+    UserApiController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/users")
+    List<User> all(@RequestParam(required = false) String method, @RequestParam(required = false) String text) {
+        List<User> users = null;
+        if ("query".equals(method)) {
+            users = repository.findByUsernameQuery(text);
+        } else if("nativeQuery".equals(method)) {
+            users = repository.findByUsernameNativeQuery(text);
+//        } else if("querydsl".equals(method)){
+//            QUs user = QUser.user;
+//            Predicate predicate = user.username.contains(text);
+//            repository.findAll(predicate);
+        } else {
+            users = repository.findAll();
+        }
+        return users;
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/users/{id}")
+    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(user -> {
+//                    user.setTitle(newUser.getTitle());
+//                    user.setContent(newUser.getContent());
+                    user.setBoards(newUser.getBoards());
+                    for(Board board : user.getBoards()) {
+                        board.setUser(user);
+                    }
+                    return repository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
